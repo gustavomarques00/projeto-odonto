f810b48e3847a2bda20a9470c99eed9ce612dcae
Gustavo Marques de Lima <gustavomarquesmm@gmail.com>
2023-02-23 23:23:46 -0300

    First Commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const HomePage = () => {
+  return (
+    <div>
+      <h1>Hello, Next.js + Vite + TypeScript! uhullll</h1>
+    </div>
+  );
+};
+
+export default HomePage;
